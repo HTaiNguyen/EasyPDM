2078da005449cd723ced80c4782a29044a0b9928
Tai <Tai@Tai-laptop>
2015-01-30 17:00:37 +0100

    Ajout des classes beans sans annotations et sans constructeurs

---
+++ EasyPDM/src/main/java/fr/upem/entity/Element.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.upem.entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Tai
+ */
+@Entity
+public class Element implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String creator;
+    private String lastEditor;
+    private boolean session;
+    private Long workSpaceID;
+    private Timestamp editStamp;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getLastEditor() {
+        return lastEditor;
+    }
+
+    public void setLastEditor(String lastEditor) {
+        this.lastEditor = lastEditor;
+    }
+
+    public boolean isSession() {
+        return session;
+    }
+
+    public void setSession(boolean session) {
+        this.session = session;
+    }
+
+    public Long getWorkSpaceID() {
+        return workSpaceID;
+    }
+
+    public void setWorkSpaceID(Long workSpaceID) {
+        this.workSpaceID = workSpaceID;
+    }
+
+    public Timestamp getEditStamp() {
+        return editStamp;
+    }
+
+    public void setEditStamp(Timestamp editStamp) {
+        this.editStamp = editStamp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Element)) {
+            return false;
+        }
+        Element other = (Element) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "fr.upem.entity.Element[ id=" + id + " ]";
+    }
+    
+}
---
+++ EasyPDM/src/main/java/fr/upem/entity/WorkPackage.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.upem.entity;
+
+import fr.upem.entity.more.Maturity;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Tai
+ */
+@Entity
+public class WorkPackage implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private long version;
+    private Maturity maturity;
+    private String creator;
+    private String lastEdit;
+    private Long workSpaceID;
+    private Timestamp editStamp;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+
+    public Maturity getMaturity() {
+        return maturity;
+    }
+
+    public void setMaturity(Maturity maturity) {
+        this.maturity = maturity;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getLastEdit() {
+        return lastEdit;
+    }
+
+    public void setLastEdit(String lastEdit) {
+        this.lastEdit = lastEdit;
+    }
+
+    public Long getWorkSpaceID() {
+        return workSpaceID;
+    }
+
+    public void setWorkSpaceID(Long workSpaceID) {
+        this.workSpaceID = workSpaceID;
+    }
+
+    public Timestamp getEditStamp() {
+        return editStamp;
+    }
+
+    public void setEditStamp(Timestamp editStamp) {
+        this.editStamp = editStamp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof WorkPackage)) {
+            return false;
+        }
+        WorkPackage other = (WorkPackage) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "fr.upem.entity.WorkPackage[ id=" + id + " ]";
+    }
+    
+}
