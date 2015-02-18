262fdd0cde3905e8e4c6e67d62ca51156082942e
Denis <Denis@PC-Denis>
2015-02-18 18:32:51 +0100

    test + Entity

--- EasyPDM/src/main/java/fr/upem/easypdm/entity/Element.java
+++ EasyPDM/src/main/java/fr/upem/easypdm/entity/Element.java
@@ -9,8 +9,6 @@ import fr.upem.entity.easypdm.more.Maturity;
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.Objects;
-import java.util.Set;
-import static javax.persistence.CascadeType.ALL;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -21,7 +19,6 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
 /**
