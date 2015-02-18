125f6dab80c5a1ca40f78fdb9b1dafba73a12f13
Miguel <Miguel@Miguel-Bureau>
2015-02-18 18:34:06 +0100

    Ajout des controllers

--- EasyPDM/src/main/java/fr/upem/controller/ElementManager.java
+++ EasyPDM/src/main/java/fr/upem/controller/ElementManager.java
@@ -5,11 +5,20 @@
  */
 package fr.upem.controller;
 
+import fr.upem.easypdm.dao.implement.ElementDAO;
+import fr.upem.easypdm.entity.Element;
+
 
 /**
  *
  * @author Denis
  */
 public class ElementManager {
- 
+    
+    private static final ElementDAO elementDAO = new ElementDAO();
+    
+    public static void createElement(Element element) {
+        elementDAO.create(element);
+    }
+    
 }
--- EasyPDM/src/main/java/fr/upem/controller/UserManager.java
+++ EasyPDM/src/main/java/fr/upem/controller/UserManager.java
@@ -5,10 +5,55 @@
  */
 package fr.upem.controller;
 
+import fr.upem.easypdm.dao.implement.UsersDAO;
+import fr.upem.easypdm.entity.Organisation;
+import fr.upem.easypdm.entity.Role;
+import fr.upem.easypdm.entity.Users;
+import fr.upem.security.RACWriter;
+import java.util.List;
+
 /**
  *
  * @author Denis
  */
 public class UserManager {
     
+    private static final UsersDAO usersDAO = new UsersDAO();
+    private final Users user;
+    //private final ROAWriter roaWriter; 
+    
+    public UserManager(Users user) {
+        this.user = user;
+        //roaWriter = new ROAWriter()
+    }
+    
+    public void addUser(Users user) {
+        
+        usersDAO.create(user);
+    }
+    
+    public void removeUser(Users user) {
+        usersDAO.remove(user);
+    }
+
+    public void updateUser(Users user) {
+        usersDAO.update(user);
+    }
+    
+    public List<Users> getAllUsers() {
+        return usersDAO.findAll();
+    }
+    
+    public List<Users> getOrganisationUsers(Organisation org) {
+        //TODO
+        return null;
+    }
+    
+    public void addUserRoleInOrganisation(Users user, Role role, Organisation org) {
+        //TODO
+    }
+    
+    public Users getUser(long id) {
+        return usersDAO.find(id);
+    }
 }
