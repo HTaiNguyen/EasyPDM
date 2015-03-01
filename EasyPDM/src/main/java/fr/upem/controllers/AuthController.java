36cecff0b5542481508021ef2d0e04747b0fcb7a
Tai <Tai@Tai-PC>
2015-03-01 15:32:13 +0100

    Page jsf connection

--- EasyPDM/src/main/java/fr/upem/controllers/AuthController.java
+++ EasyPDM/src/main/java/fr/upem/controllers/AuthController.java
@@ -67,7 +67,15 @@ public class AuthController implements Serializable{
         return userName;
     }
     
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
     public String getPassword() {
         return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
